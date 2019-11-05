eda5871af831507cb08f5199e6ea5a26fb6dc96b
DuongThaiSon <sontd@leotive.com>
2019-11-05 14:56:59 +0700

    sửa giao diện

--- resources/views/client/app.blade.php
+++ resources/views/client/app.blade.php
@@ -2,98 +2,59 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
 
-		<!-- Loading third party fonts -->
-		<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
-		<link href="{{ asset('client/fonts/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Loading third party fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+    <link href="{{ asset('client/fonts/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-	<!-- Loading third party fonts -->
-	<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
-	<link href="{{ asset('client/fonts/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<!-- Loading main css file -->
-	<link rel="stylesheet" href="{{ asset('client/style.css') }}">
-
-	<!--[if lt IE 9]>
-		<script src="js/ie-support/html5.js"></script>
-		<script src="js/ie-support/respond.js"></script>
-		<![endif]-->
+    <!-- Loading third party fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+    <link href="{{ asset('client/fonts/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Loading main css file -->
+    <link rel="stylesheet" href="{{ asset('client/style.css') }}">
+    <script src="https://kit.fontawesome.com/540cf737fa.js" crossorigin="anonymous"></script>
 
 </head>
 
 
 <body>
+    <div class="site-content">
+        <div class="site-header">
+            <div class="container">
+                <a href="/" class="branding">
+                    <img src="{{ asset('client/images/logo.png') }}" alt="" class="logo">
+                    <div class="logo-type">
+                        <h1 class="site-title">Weather Forecast</h1>
+                        <small class="site-description">Software Technology</small>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    @yield('content')
 
-	<div class="site-content">
-		<div class="site-header">
-			<div class="container">
-				<a href="/" class="branding">
-					<img src="{{ asset('client/images/logo.png') }}" alt="" class="logo">
-					<div class="logo-type">
-						<h1 class="site-title">Company name</h1>
-						<small class="site-description">tagline goes here</small>
-					</div>
-				</a>
+    <script src="{{ asset('client/js/jquery-1.11.1.min.js') }}"></script>
+    <script src="{{ asset('client/js/plugins.js') }}"></script>
+    <script src="{{ asset('client/js/app.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
 
-				<!-- Default snippet for navigation -->
-				<div class="main-navigation">
-					<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-					<ul class="menu">
-						<li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
-						<li class="menu-item"><a href="news.html">News</a></li>
-						<li class="menu-item"><a href="live-cameras.html">Live cameras</a></li>
-						<li class="menu-item"><a href="photos.html">Photos</a></li>
-						<li class="menu-item"><a href="contact.html">Contact</a></li>
-					</ul> <!-- .menu -->
-				</div> <!-- .main-navigation -->
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-				<div class="mobile-navigation"></div>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
-			</div>
-		</div> <!-- .site-header -->
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-		@yield('content')
-
-		<footer class="site-footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8">
-						<form action="#" class="subscribe-form">
-							<input type="text" placeholder="Enter your email to subscribe...">
-							<input type="submit" value="Subscribe">
-						</form>
-					</div>
-					<div class="col-md-3 col-md-offset-1">
-						<div class="social-links">
-							<a href="#"><i class="fa fa-facebook"></i></a>
-							<a href="#"><i class="fa fa-twitter"></i></a>
-							<a href="#"><i class="fa fa-google-plus"></i></a>
-							<a href="#"><i class="fa fa-pinterest"></i></a>
-						</div>
-					</div>
-				</div>
-
-		<script src="{{ asset('client/js/jquery-1.11.1.min.js') }}"></script>
-		<script src="{{ asset('client/js/plugins.js') }}"></script>
-		<script src="{{ asset('client/js/app.js') }}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
-
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-        <!-- Popper JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-        <script src="{{ asset('client/script.js') }}"></script>
-        @stack('scripts')
-	</body>
+    <script src="{{ asset('client/script.js') }}"></script>
+    @stack('scripts')
+</body>
 
 </html>
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 
-<div class="hero" data-bg-image="{{ asset('client/images/banner.png') }}">
+<div class="hero" data-bg-image="{{ asset('client/images/banner1.jpg') }}">
     <div class="container">
         <form action="/" class="find-location" id="header-search" autocomplete="off">
-            <input autocomplete="off" type="text" class="search-input" name="search" placeholder="Find your location...">
-            <!-- <input type="submit" value="Find"> -->
+            <input autocomplete="off" type="text" class="search-input" name="search"
+                placeholder="Find your location...">
+            <button type="submit" class="btn-search">
+                <i class="fas fa-search"></i>
+            </button>
             <div id="result">
             </div>
         </form>
     </div>
-</div>
 <div class="forecast-table">
     <div class="container main-daily">
         <div class="forecast-container">
@@ -36,7 +38,6 @@
                         </div>
                         <span><img src="images/icon-umberella.png" alt="">{{ $daily->rh }}%</span>
                         <span><img src="images/icon-wind.png" alt="">{{ $daily->wind_dir }}km/h</span>
-                        <!-- <span><img src="images/icon-compass.png" alt="">East</span> -->
                     </div>
                     <div class="notToday">
                         <div class="forecast-icon">
@@ -47,22 +48,22 @@
                 </div>
                 <div class="hourly">
                     @foreach($daily->hourly as $keyHourly => $hourly)
-                        <div class="hourly-item"  data-toggle="modal" data-target="#hourly{{$hourly->id}}">
-                            <div class="hourly-title">
-                                <p>{{ $hourly->hour }}</p>
-                            </div>
-                            <div class="hourly-content">
-                                <h2>{{ $hourly->temp }}<sup>o</sup>C</h2>
-                            </div>
-                            <div class="hourly-footer">
-                                <img src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" /></img>
-                            </div>
+                    <div class="hourly-item" data-toggle="modal" data-target="#hourly{{$hourly->id}}">
+                        <div class="hourly-title">
+                            <p>{{ $hourly->hour }}</p>
                         </div>
+                        <div class="hourly-content">
+                            <h2>{{ $hourly->temp }}<sup>o</sup>C</h2>
+                        </div>
+                        <div class="hourly-footer">
+                            <img src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" />
+                        </div>
+                    </div>
 
-                        <!-- The Modal -->
-                        <div class="modal" id="hourly{{$hourly->id}}" style="color: #000">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
+                    <!-- The Modal -->
+                    <div class="modal" id="hourly{{$hourly->id}}" style="color: #000">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
 
                                 <!-- Modal Header -->
                                 <div class="modal-header">
@@ -72,8 +73,7 @@
 
                                 <!-- Modal body -->
                                 <div class="modal-body">
-
-                                    <img src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" /></img>
+                                    <img src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" />
                                     <p>Độ ẩm tương đối: {{ $hourly->rh }}%</p>
                                     <p>Tốc độ gió: {{ $hourly->wind_spd }}m/s</p>
                                     <p>Áp suất: {{ $hourly->press }}</p>
@@ -91,9 +91,9 @@
                                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                                 </div>
 
-                                </div>
                             </div>
                         </div>
+                    </div>
 
                     @endforeach
                 </div>
@@ -127,22 +127,23 @@
                 </div>
                 <div class="hourly">
                     @foreach($daily->hourly as $hourly)
-                        <div class="hourly-item"  data-toggle="modal" data-target="#hourly{{$hourly->id}}">
-                            <div class="hourly-title">
-                                <p>{{ $hourly->hour }}</p>
-                            </div>
-                            <div class="hourly-content">
-                                <h2>{{ $hourly->temp }}<sup>o</sup>C</h2>
-                            </div>
-                            <div class="hourly-footer">
-                                <img src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" /></img>
-                            </div>
+                    <div class="hourly-item" data-toggle="modal" data-target="#hourly{{$hourly->id}}">
+                        <div class="hourly-title">
+                            <p>{{ $hourly->hour }}</p>
                         </div>
+                        <div class="hourly-content">
+                            <h2>{{ $hourly->temp }}<sup>o</sup>C</h2>
+                        </div>
+                        <div class="hourly-footer">
+                            <img
+                                src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" />
+                        </div>
+                    </div>
 
-                        <!-- The Modal -->
-                        <div class="modal" id="hourly{{$hourly->id}}" style="color: #000">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
+                    <!-- The Modal -->
+                    <div class="modal" id="hourly{{$hourly->id}}" style="color: #000">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
 
                                 <!-- Modal Header -->
                                 <div class="modal-header">
@@ -153,7 +154,8 @@
                                 <!-- Modal body -->
                                 <div class="modal-body">
 
-                                    <img src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" /></img>
+                                    <img
+                                        src="https://www.weatherbit.io/static/img/icons/{{ $hourly->weather_json->icon }}.png" />
                                     <p>Độ ẩm tương đối: {{ $hourly->rh }}%</p>
                                     <p>Tốc độ gió: {{ $hourly->wind_spd }}m/s</p>
                                     <p>Áp suất: {{ $hourly->press }}</p>
@@ -171,9 +173,9 @@
                                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                                 </div>
 
-                                </div>
                             </div>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
@@ -186,106 +188,43 @@
 
     </div>
 </div>
-<main class="main-content">
-    <div class="fullwidth-block">
-        <div class="container">
-            <h2 class="section-title">Live cameras</h2>
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <div class="live-camera">
-                        <figure class="live-camera-cover"><img src="images/live-camera-1.jpg" alt=""></figure>
-                        <h3 class="location">New York</h3>
-                        <small class="date">8 oct, 8:00AM</small>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="live-camera">
-                        <figure class="live-camera-cover"><img src="images/live-camera-2.jpg" alt=""></figure>
-                        <h3 class="location">Los Angeles</h3>
-                        <small class="date">8 oct, 8:00AM</small>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="live-camera">
-                        <figure class="live-camera-cover"><img src="images/live-camera-3.jpg" alt=""></figure>
-                        <h3 class="location">Chicago</h3>
-                        <small class="date">8 oct, 8:00AM</small>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="live-camera">
-                        <figure class="live-camera-cover"><img src="images/live-camera-4.jpg" alt=""></figure>
-                        <h3 class="location">London</h3>
-                        <small class="date">8 oct, 8:00AM</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="fullwidth-block" data-bg-color="#262936">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="news">
-                        <div class="date">06.10</div>
-                        <h3><a href="#">Doloremque laudantium totam sequi </a></h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo saepe assumenda dolorem modi, expedita voluptatum ducimus necessitatibus. Asperiores quod reprehenderit necessitatibus harum, mollitia, odit et consequatur maxime nisi amet doloremque.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="news">
-                        <div class="date">06.10</div>
-                        <h3><a href="#">Doloremque laudantium totam sequi </a></h3>
-                        <p>Nobis architecto consequatur ab, ea, eum autem aperiam accusantium placeat vitae facere explicabo temporibus minus distinctio cum optio quis, dignissimos eius aspernatur fuga. Praesentium totam, corrupti beatae amet expedita veritatis.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="news">
-                        <div class="date">06.10</div>
-                        <h3><a href="#">Doloremque laudantium totam sequi </a></h3>
-                        <p>Enim impedit officiis placeat qui recusandae doloremque possimus, iusto blanditiis, quam optio delectus maiores. Possimus rerum, velit cum natus eos. Cumque pariatur beatae asperiores, esse libero quas ad dolorem. Voluptates.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</main> <!-- .main-content -->
-
+</div>
 @endsection
 
 @push("scripts")
 <script type="text/javascript">
-   $('#header-search').on('keyup', function() {
-       var search = $(this).find('.search-input').val();
-       if (search == '') {
-        $("#result").hide()
-       } else {
-           axios.get('/search', {
-               params: {
-                search: search
-               }
-           })
-           .then(res => {
-               let html = "";
-               res.data.result.forEach(e => {
-                html += `<p>${e.name}</p>`;
-               });
-               console.log(html);
-               $("#result").show();
-               $("#result").html(html);
+    $('#header-search').on('keyup', function () {
+        var search = $(this).find('.search-input').val();
+        if (search == '') {
+            $("#result").hide()
+        } else {
+            axios.get('/search', {
+                    params: {
+                        search: search
+                    }
+                })
+                .then(res => {
+                    let html = "";
+                    res.data.result.forEach(e => {
+                        html += `<p>${e.name}</p>`;
+                    });
+                    console.log(html);
+                    $("#result").show();
+                    $("#result").html(html);
 
-           }).catch(err => {
-               console.log({err: err})
-           });
-       };
-   });
+                }).catch(err => {
+                    console.log({
+                        err: err
+                    })
+                });
+        };
+    });
 
-   $(".forecast").click(function(e) {
-       e.preventDefault();
-       $('.forecast').removeClass('today');
-    $(this).addClass("today");
-   })
+    $(".forecast").click(function (e) {
+        e.preventDefault();
+        $('.forecast').removeClass('today');
+        $(this).addClass("today");
+    })
+
 </script>
 @endpush
